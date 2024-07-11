8de0a0712a28d128a7aea4b8945f6c2bc87da13e
mei <mei28aquarius@gmail.com>
2024-07-11 16:26:51 +0900

    feat: Add state persistence and control commands to qfc.nvim
    This commit introduces several enhancements to the qfc.nvim plugin:
    
    1. State Persistence: The plugin now retains its state between Neovim
       sessions. This is achieved by saving the plugin's state to a file
       whenever it changes, and loading it when the plugin is initialized.
    
    2. Control Commands: Three new commands have been added to control the
       plugin's behavior:
       - :EnableQFC: Enables the Quickfix autoclose feature.
       - :DisableQFC: Disables the Quickfix autoclose feature.
       - :ToggleQFC: Toggles the Quickfix autoclose feature.
    
    These changes provide users with more control over the plugin's behavior
    and improve the user experience by making the plugin's state persistent
    across sessions.

--- README.md
+++ README.md
@@ -2,14 +2,13 @@
 
 <img src="https://github.com/mei28/qfc.nvim/assets/51149822/2997f892-d2ba-40ae-9870-d52fdbc45deb" alt="qfc" width="500"/>
 
-
-
 `qfc.nvim` is a Neovim plugin that automatically manages Quickfix windows. It closes the Quickfix window after a specified timeout when the window loses focus and deletes the buffer.
 
 ## Features
 
 - Automatically closes the Quickfix window a specified time after it loses focus.
 - Deletes the Quickfix buffer after closing the window.
+- Retains the plugin state between Neovim sessions.
 
 ## Installation
 
@@ -20,7 +19,9 @@
     require('qfc').setup({
       timeout = 3000,   -- Timeout setting in milliseconds
       autoclose = true, -- Enable/disable autoclose feature
-    })
+    }),
+   -- ft = 'qf', -- for lazy load
+   -- cmd = {'EnableQFC', 'DisableQFC', 'ToggleQFC'} -- for lazy load 
   end
 }
 ```
@@ -30,14 +31,18 @@
 ```lua
 require('qfc').setup({
   timeout = 2000,   -- Timeout setting in milliseconds
-  autoclose = true, -- Enable/disable autoclose feature
+  enabled = true, -- Enable/disable autoclose feature
 })
 ```
 
 ### Configuration Options
-
-- timeout: Specifies the timeout in milliseconds after which the Quickfix window will be automatically closed once it loses focus.
-- autoclose: Enables or disables the autoclose feature. Set to true to enable or false to disable.
+* timeout: Specifies the timeout in milliseconds after which the Quickfix window will be automatically closed once it loses focus.
+* enabled: Enables or disables the plugin. Set to true to enable or false to disable.
+ 
+### Commands
+* `:EnableQFC` Enables the Quickfix autoclose feature.
+* `:DisableQFC` Disables the Quickfix autoclose feature.
+* `:ToggleQFC` toggle the Quickfix autoclose feature.
 
 ## License
 
--- lua/qfc/init.lua
+++ lua/qfc/init.lua
@@ -2,16 +2,51 @@ local M = {}
 
 -- Default configuration
 M.config = {
-  timeout = 3000,   -- Default timeout of 3000 milliseconds
-  autoclose = true, -- Enable autoclose by default
+  timeout = 3000, -- Default timeout of 3000 milliseconds
+  enabled = true, -- Enable the plugin by default
 }
 
 -- Timer to handle auto close
 local timer = vim.loop.new_timer()
 
+-- Function to get the path for the config file
+local function get_config_path()
+  return vim.fn.stdpath('data') .. '/qfc_cache'
+end
+
+-- Function to save the current configuration to a file
+local function save_config()
+  local config_path = get_config_path()
+  local file = io.open(config_path, "w")
+  if file then
+    file:write(M.config.enabled and "true" or "false")
+    file:close()
+  else
+    vim.api.nvim_err_writeln("Failed to save config to " .. config_path)
+  end
+end
+
+-- Function to load the configuration from a file
+local function load_config()
+  local config_path = get_config_path()
+  local file = io.open(config_path, "r")
+  if file then
+    local content = file:read("*a")
+    file:close()
+    if content == "true" then
+      M.config.enabled = true
+    elseif content == "false" then
+      M.config.enabled = false
+    end
+  else
+    -- If the file does not exist, save the default configuration
+    save_config()
+  end
+end
+
 -- Function to start the timer
 function M.start_qf_timer()
-  if vim.bo.buftype == 'quickfix' and M.config.autoclose then
+  if vim.bo.buftype == 'quickfix' and M.config.enabled then
     local bufnr = vim.fn.bufnr()
     local winid = vim.fn.win_getid()
     timer:start(M.config.timeout, 0, vim.schedule_wrap(function()
@@ -34,9 +69,28 @@ function M.stop_qf_timer()
   end
 end
 
+-- Function to enable the plugin
+function M.enable()
+  M.config.enabled = true
+  save_config()
+end
+
+-- Function to disable the plugin
+function M.disable()
+  M.config.enabled = false
+  save_config()
+end
+
+-- Function to toggle the plugin
+function M.toggle()
+  M.config.enabled = not M.config.enabled
+  save_config()
+end
+
 -- Setup function to allow user configuration
 function M.setup(user_config)
   M.config = vim.tbl_deep_extend('force', M.config, user_config or {})
+  load_config()
 
   -- Set up autocommands for quickfix window
   vim.api.nvim_create_autocmd('WinLeave', {
@@ -56,6 +110,11 @@ function M.setup(user_config)
       end
     end,
   })
+
+  -- Create user commands
+  vim.api.nvim_create_user_command('EnableQFC', function() M.enable() end, {})
+  vim.api.nvim_create_user_command('DisableQFC', function() M.disable() end, {})
+  vim.api.nvim_create_user_command('ToggleQFC', function() M.toggle() end, {})
 end
 
 return M
